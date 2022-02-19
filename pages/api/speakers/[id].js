e024e089b374f0eb0c7e151749fffbd6ac1dde42
tomideadeoye <tommideadeoye@gmail.com>
2022-02-19 13:10:01 +0100

    Fixed 404 issue

--- pages/api/speakers/[id].js
+++ pages/api/speakers/[id].js
@@ -36,20 +36,20 @@ export default async function handler(req, res) {
 			const readFileData = await readFile(jsonFile);
 			await delay(1000);
 			const speakers = JSON.parse(readFileData).speakers;
-			if (!speakers) {
-				res.status(404).send("Error: Request failed with status code 404");
-			} else {
-				const newSpeakersArray = speakers.map(function (rec) {
-					return rec.id == id ? recordFromBody : rec;
-				});
-				writeFile(
-					jsonFile,
-					JSON.stringify({ speakers: newSpeakersArray }, null, 2)
-				);
-				res.setHeader("Content-Type", "application/json");
-				res.status(200).send(JSON.stringify(recordFromBody, null, 2));
-				console.log(`PUT /api/speakers/${id}  status: 200`);
-			}
+			// if (!speakers) {
+			// 	res.status(404).send("Error: Request failed with status code 404");
+			// } {else }
+
+			const newSpeakersArray = speakers.map(function (rec) {
+				return rec.id == id ? recordFromBody : rec;
+			});
+			writeFile(
+				jsonFile,
+				JSON.stringify({ speakers: newSpeakersArray }, null, 2)
+			);
+			res.setHeader("Content-Type", "application/json");
+			res.status(200).send(JSON.stringify(recordFromBody, null, 2));
+			console.log(`PUT /api/speakers/${id}  status: 200`);
 		} catch (e) {
 			res
 				.status(500)
@@ -63,26 +63,25 @@ export default async function handler(req, res) {
 			const readFileData = await readFile(jsonFile);
 			await delay(1000);
 			const speakers = JSON.parse(readFileData).speakers;
-			if (!speakers) {
-				res.status(404).send("Error: Request failed with status code 404");
-			} else {
-				const newSpeakersArray = speakers.filter(function (rec) {
-					return rec.id != id;
-				});
-				writeFile(
-					jsonFile,
-					JSON.stringify({ speakers: newSpeakersArray }, null, 2)
-				);
-				res.setHeader("Content-Type", "application/json");
-				res.status(200).send(
-					JSON.stringify(
-						speakers.find((rec) => rec.id == id),
-						null,
-						2
-					)
-				);
-				console.log(`DELETE /api/speakers/${id}  status: 200`);
-			}
+			// if (!speakers) {
+			// 	res.status(404).send("Error: Request failed with status code 404");
+			// } else {}
+			const newSpeakersArray = speakers.filter(function (rec) {
+				return rec.id != id;
+			});
+			writeFile(
+				jsonFile,
+				JSON.stringify({ speakers: newSpeakersArray }, null, 2)
+			);
+			res.setHeader("Content-Type", "application/json");
+			res.status(200).send(
+				JSON.stringify(
+					speakers.find((rec) => rec.id == id),
+					null,
+					2
+				)
+			);
+			console.log(`DELETE /api/speakers/${id}  status: 200`);
 		} catch (e) {
 			res
 				.status(500)
@@ -96,26 +95,25 @@ export default async function handler(req, res) {
 			const readFileData = await readFile(jsonFile);
 			await delay(1000);
 			const speakers = JSON.parse(readFileData).speakers;
-			if (!speakers) {
-				res.status(404).send("Error: Request failed with status code 404");
-			} else {
-				const idNew =
-					speakers.reduce((accumulator, currentValue) => {
-						const idCurrent = parseInt(currentValue.id);
-						return idCurrent > accumulator ? idCurrent : accumulator;
-					}, 0) + 1;
+			// if (!speakers) {
+			// 	res.status(404).send("Error: Request failed with status code 404");
+			// } else {}
+			const idNew =
+				speakers.reduce((accumulator, currentValue) => {
+					const idCurrent = parseInt(currentValue.id);
+					return idCurrent > accumulator ? idCurrent : accumulator;
+				}, 0) + 1;
 
-				const newSpeakerRec = { ...recordFromBody, id: idNew.toString() };
+			const newSpeakerRec = { ...recordFromBody, id: idNew.toString() };
 
-				const newSpeakersArray = [newSpeakerRec, ...speakers];
-				writeFile(
-					jsonFile,
-					JSON.stringify({ speakers: newSpeakersArray }, null, 2)
-				);
-				res.setHeader("Content-Type", "application/json");
-				res.status(200).send(JSON.stringify(newSpeakerRec, null, 2));
-				console.log(`POST /api/speakers/${id}  status: 200`);
-			}
+			const newSpeakersArray = [newSpeakerRec, ...speakers];
+			writeFile(
+				jsonFile,
+				JSON.stringify({ speakers: newSpeakersArray }, null, 2)
+			);
+			res.setHeader("Content-Type", "application/json");
+			res.status(200).send(JSON.stringify(newSpeakerRec, null, 2));
+			console.log(`POST /api/speakers/${id}  status: 200`);
 		} catch (e) {
 			res
 				.status(500)
